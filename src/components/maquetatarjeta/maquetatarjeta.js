b8ab3cee4a079facdc34c9a0a3ad4bd5626ca61a
BelenVincent <beluvincent@hotmail.com>
2021-09-09 20:59:34 -0300

    ver mas

--- src/components/maquetatarjeta/maquetatarjeta.js
+++ src/components/maquetatarjeta/maquetatarjeta.js
@@ -7,19 +7,47 @@ export default class Maqueta extends Component {
     eliminar(name){
         const artistasFiltrados = this.state.artistas.filter(character => character.name !== name)
 
+    };
+
+    constructor(props){
+        super(props);
+        this.state={
+           clase: "hide",
+           mensaje: "ver más"
     }
+
+
+}
+
+// funcion llamada cuando se hace click en ver mas o ver menos
+handleShow(){
+    if (this.state.clase === 'hide'){
+        this.setState({
+            clase: 'show',
+            mensaje: "ver menos"
+        })
+        } else {
+        this.setState({
+            clase: 'hide',
+            mensaje: "ver más"
+        })   
+    }
+}
 
     render() {
         return (
             <div className = 'maqueta'>
                 <img src={this.props.foto} alt="" ></img>
                 <p>{this.props.name}</p>
-                <button type="submit"> ver mas </button>
-                <p>This {this.props.type} is on position {this.props.position} </p>
-                <a href={this.props.link}>conoce mas del artista</a>
-                <button onClick={() => this.props.removerArtista(this.props.name)}> eliminar artista </button>
+                <button type="submit" onClick={() => this.handleShow()}>{this.state.mensaje} </button>
+                <p className={this.state.clase}>This {this.props.type} is on position {this.props.position} </p>
+                <p className={this.state.clase}> <a href={this.props.link}>conoce mas del artista</a></p>
+                <p className={this.state.clase}>  <button onClick={() => this.props.removerArtista(this.props.name)}> eliminar artista </button></p>
 
             </div>
-        )
+        );
     }
 }
+
+
+

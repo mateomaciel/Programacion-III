f1a69a707c7028fd08f32352551d278a0928e50b
santiago-genoni <sgenoni@udesa.edu.ar>
2021-09-10 12:11:15 -0300

    buscador

---
+++ src/components/buscador/Buscador.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class Buscador extends Component {
+    constructor(){
+        super();
+        this.state = {
+            value : ""
+        }   
+    }
+
+    PrevenirDefault(evento){
+    evento.PreventDefault()
+    }
+    capturaInput(evento){
+       this.setState(
+            {value: evento.target.value},
+            ()=> this.props.FiltrarPorNombre(this.state.value)
+        )
+    }
+    render(){
+        return(<div>
+            <form onSubmit={(evento)=> this.PrevenirDefault(evento)}>
+                <input onChange={(evento)=> this.capturaInput(evento)}type="text"/>
+            </form>
+            <h1>{this.state.value}</h1>
+            </div>
+        )
+    }
+}
--- src/components/canciones/canciones.js
+++ src/components/canciones/canciones.js
@@ -1,13 +1,14 @@
 import React, { Component} from 'react';
 import Maqueta from '../maquetatarjeta/maquetatarjeta'
-
+import Buscador from '../buscador/Buscador'
 
 class Tarjeta extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            artistas:[]
+            artistas:[],
+            NombreFiltrado:[]
         }
     }
 
@@ -15,11 +16,25 @@ class Tarjeta extends Component {
         fetch('https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/artists')
         .then(function(response) {return response.json();})
         .then(data => {console.log(data);
-            this.setState({artistas:data.data});
+            this.setState({artistas:data.data, NombreFiltrado: data.data});
 
 
         })
     }
+
+    FiltrarPorNombre(NombreAFiltrar){
+        const ArrayFiltrado = this.state.artistas.filter(
+            milanesa=> milanesa.name.toLowerCase().includes(NombreAFiltrar)
+            
+        )
+    
+        if(NombreAFiltrar === ""){
+            this.setState({NombreFiltrado: this.state.artistas})
+        }
+        else{this.setState({
+            NombreFiltrado: ArrayFiltrado
+        })}
+        }
 
     removerArtista(name) {
 
@@ -31,8 +46,9 @@ class Tarjeta extends Component {
             console.log(this.state.artistas)
 
         return(
-        <div>            
-           {this.state.artistas.map( (artistas, index) => {
+        <div>   
+            <Buscador FiltrarPorNombre={(NombreAFiltrar)=> this.FiltrarPorNombre(NombreAFiltrar)}/>         
+           {this.state.NombreFiltrado.map( (artistas, index) => {
                return <Maqueta key={index} 
                link={artistas.link} 
                name = {artistas.name} 
